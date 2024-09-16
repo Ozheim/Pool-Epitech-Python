a1ab48bd3773921c6d1b692473ba15c0861caead
Ozheim <heikel.zouaoui@gmail.com>
2024-09-16 17:22:48 +0200

    day finished

--- day 6/script.py
+++ day 6/script.py
@@ -101,7 +101,62 @@ def palindrome(mot):
             mot= mot.replace(lettre,"")
         mot= mot.replace(" ", "")   
  
-    print(mot)
+    return print(mot == mot[::-1])
 
 
-palindrome("Ka /yak")
+palindrome("never odd or even")
+
+#task 3.1 
+
+def funA(s,n):
+    total = 0
+    
+    for letter in s: 
+        total += letter.islower()
+    if total >= n: 
+        return True
+    else:
+        return False
+    
+#print(funA("sssss",2))
+
+def funB(s,n):
+    total = 0
+    for letter in s: 
+        total += letter.isupper()
+    return total >= n
+    
+#print(funB("hello world", 2))
+
+def funC(s,n):
+    total = 0
+    for letter in s: 
+        total += len(letter)
+    return total >= n
+
+#print(funC("lalalal",5 ))
+
+
+sp = "!@#$%^&*()-+?_=,<>"
+def funD(s,n): 
+    total =0
+    for car in s: 
+        if car in sp:
+            total += 1
+    return total>=n
+
+#print( funD( "strin!", 2))
+
+def funE(x,y):
+    total = 0
+    for number in x:
+        total += number.isnumeric()
+    if total >= y:
+       return True
+    else :
+        return False
+
+#print(funE("dsmsp8989", 4))
+
+
+
